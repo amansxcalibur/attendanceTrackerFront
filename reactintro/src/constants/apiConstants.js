a76679562e5c16cc00a138c4b6d6ec1b2167f56e
amansxcalibur <notifyamanvs@gmail.com>
2024-06-05 21:31:05 +0530

    Fresh Start V2

--- reactintro/src/constants/apiConstants.js
+++
@@ -1,2 +0,0 @@
-export const API_BASE_URL = "https://api.bunkmate.college";
-export const ACCESS_TOKEN_NAME = 'login_access_token';
--- reactintro/src/constants/colors.js
+++
@@ -1,2 +0,0 @@
-export const table_color="#181a1c";
-export const light="#6eb898";
